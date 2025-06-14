09c63ee92537894ac0c319a2f814ab48cb3b6910
Murilo Henrique Russo <79058778+MuriloRusso@users.noreply.github.com>
2025-06-14 13:41:53 -0300

    created types

--- front/src/types/exam.tsx
+++ front/src/types/exam.tsx
@@ -1,3 +1,7 @@
 export type Exam = {
-    
+    id: number;
+    name: string;
+    specialty: string;
+    created_at: string;
+    updated_at: string;
 }
--- front/src/types/scheduling.tsx
+++ front/src/types/scheduling.tsx
@@ -1,3 +1,9 @@
 export type Scheduling = {
-    
+    id: number;
+    id_exam: number;
+    speciality: string;
+    date_time: string;
+    informations: string;
+    created_at: string;
+    updated_at: string;
 }
