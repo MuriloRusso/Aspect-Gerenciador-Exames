cbfaf2b8baeb7ee619bc64d28e850f99703f84c9
Murilo Henrique Russo <79058778+MuriloRusso@users.noreply.github.com>
2025-06-18 15:32:06 -0300

    mask in created_at in useExamsColumns

--- front/src/hooks/useExamsColumns/index.tsx
+++ front/src/hooks/useExamsColumns/index.tsx
@@ -1,10 +1,19 @@
 import { GridColDef } from "@mui/x-data-grid";
+import { format } from "date-fns";
 
 export default function useExamColumns() {
-    const columns: GridColDef[] = [
-        { field: 'name', headerName: 'Tipo de Exame', width: 200 },
-        { field: 'specialty', headerName: 'Especialidade', width: 250 },
-        { field: 'created_at', headerName: 'Criado', width: 150, }
-    ];
-    return {columns}
+  const columns: GridColDef[] = [
+    { field: 'name', headerName: 'Tipo de Exame', width: 200 },
+    { field: 'specialty', headerName: 'Especialidade', width: 250 },
+    { 
+      field: 'created_at', 
+      headerName: 'Criado', 
+      width: 150,
+      renderCell: (params) => {
+        if (!params.value) return '';
+        return format(new Date(params.value), "dd/MM/yyyy HH:mm");
+      }
+    }
+  ];
+  return { columns };
 }
