812c7cacb7919394842f14db8823e6187f87d420
Murilo Henrique Russo <79058778+MuriloRusso@users.noreply.github.com>
2025-06-18 12:30:18 -0300

    Created column Actions in scheduling dataGrid

--- front/src/hooks/useColumns/index.tsx
+++ front/src/hooks/useColumns/index.tsx
@@ -5,7 +5,11 @@ export default function useColumns() {
         // { field: 'id_exam', headerName: 'ID do Exame', width: 350 },
         { field: 'informations', headerName: 'Informações', width: 150 },
         { field: 'date_time', headerName: 'Data e Hora', width: 150 },
-        { field: 'created_at', headerName: 'Criado', width: 150, }
+        { field: 'created_at', headerName: 'Criado', width: 150, },
+        { field: '', headerName: 'Ações', width: 150, renderCell: function(e){
+            return <></>
+        }}
+
     ];
     return {columns}
 }
