fe8c2f8ca463eb8d4736e2bc9ebd436239d2425c
Murilo Henrique Russo <79058778+MuriloRusso@users.noreply.github.com>
2025-06-18 15:19:48 -0300

    Inclued Exams Datas in Scheduling DataGrid

--- front/src/hooks/useColumns/index.tsx
+++ front/src/hooks/useColumns/index.tsx
@@ -4,20 +4,40 @@ import useModals from "../useModals";
 import { Scheduling } from "../../types/scheduling";
 
 export default function useColumns() {
-    const { toogleModalDeleteScheduling, handleSelectedSchedulingChange } = useModals();
+  const { toogleModalDeleteScheduling, handleSelectedSchedulingChange } = useModals();
 
-    const handleDeleteScheduling = (e:Scheduling) => {       
-        handleSelectedSchedulingChange(e);
-        toogleModalDeleteScheduling();
-    }
-    const columns: GridColDef[] = [
-        { field: 'exam.id', headerName: 'ID do Exame', width: 350 },
-        { field: 'informations', headerName: 'Informações', width: 350 },
-        { field: 'date_time', headerName: 'Data e Hora', width: 250 },
-        { field: 'created_at', headerName: 'Criado', width: 250, },
-        { field: '', headerName: 'Ações', width: 100, renderCell: function(e){
-            return <TrashIcon title="Excluir" onClick={() => handleDeleteScheduling(e.row)}/>
-        }}
-    ];
-    return {columns}
+  const handleDeleteScheduling = (e: Scheduling) => {
+    handleSelectedSchedulingChange(e);
+    toogleModalDeleteScheduling();
+  };
+
+  const columns: GridColDef[] = [
+    {
+      field: 'examName',
+      headerName: 'Exame',
+      width: 250,
+      renderCell: (params) => params.row.exam?.name || '',
+    },
+    {
+      field: 'examSpecialty',
+      headerName: 'Especialidade',
+      width: 250,
+      renderCell: (params) => params.row.exam?.specialty || '',
+    },
+    { field: 'informations', headerName: 'Informações', width: 350 },
+    { field: 'date_time', headerName: 'Data e Hora', width: 250 },
+    { field: 'created_at', headerName: 'Criado', width: 250 },
+    {
+      field: 'actions',
+      headerName: 'Ações',
+      width: 100,
+      sortable: false,
+      filterable: false,
+      renderCell: (params) => (
+        <TrashIcon title="Excluir" onClick={() => handleDeleteScheduling(params.row)} />
+      ),
+    },
+  ];
+
+  return { columns };
 }
