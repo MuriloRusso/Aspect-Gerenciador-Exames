f80c5a7cad7ba4e7da65fe5ed31a177a051aa73a
Murilo Henrique Russo <79058778+MuriloRusso@users.noreply.github.com>
2025-06-18 12:34:12 -0300

    Created TrashIcon component

---
+++ front/src/components/TrashIcon/index.tsx
@@ -0,0 +1,15 @@
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import { Button } from '@mui/material';
+
+type TrashIconProps = {
+    onClick: () => void;
+    title?: string;
+}
+
+export default function TrashIcon({onClick, title}:TrashIconProps){
+    return (
+        <Button onClick={onClick} title={title ? title : ''}>
+            <DeleteOutlineOutlinedIcon/>
+        </Button>
+    )
+}
