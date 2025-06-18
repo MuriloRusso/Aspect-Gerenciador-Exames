f6d7a4939b85ca1326d04ac577aea5ce5999208e
Murilo Henrique Russo <79058778+MuriloRusso@users.noreply.github.com>
2025-06-18 12:47:27 -0300

    Inclued modalDeleteScheduling state in useModals custom hook

--- front/src/hooks/useModals/index.tsx
+++ front/src/hooks/useModals/index.tsx
@@ -2,12 +2,30 @@ import { useContext } from "react"
 import { GlobalContext } from "../../contexts/GlobalContext";
 
 export default function useModals(){
-    const { modalSchedulingOpen, setModalSchedulingOpen, modalExamsOpen, setModalExamsOpen } = useContext(GlobalContext);
+    const {
+        modalSchedulingOpen,
+        setModalSchedulingOpen,
+        modalExamsOpen,
+        setModalExamsOpen,
+        modalDeleteSchedulingOpen,
+        setModalDeleteSchedulingOpen
+    } = useContext(GlobalContext);
+
     const toogleModalScheduling = () => {
         setModalSchedulingOpen(!modalSchedulingOpen);
+    }
+    const toogleModalDeleteScheduling = () => {
+        setModalDeleteSchedulingOpen(!modalDeleteSchedulingOpen);
     }
     const toogleModalExamsList = () => {
         setModalExamsOpen(!modalExamsOpen);
     }
-    return {modalSchedulingOpen, toogleModalScheduling, modalExamsOpen, toogleModalExamsList }
+    return {
+        modalSchedulingOpen,
+        toogleModalScheduling,
+        modalExamsOpen,
+        toogleModalExamsList,
+        modalDeleteSchedulingOpen,
+        toogleModalDeleteScheduling
+    }
 }
