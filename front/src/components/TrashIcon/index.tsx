524583736576b5785dc35f894cdff9569103bbb5
Murilo Henrique Russo <79058778+MuriloRusso@users.noreply.github.com>
2025-06-18 12:37:11 -0300

    TrashIcon inclued in useColumns

--- front/src/components/TrashIcon/index.tsx
+++ front/src/components/TrashIcon/index.tsx
@@ -2,14 +2,14 @@ import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined
 import { Button } from '@mui/material';
 
 type TrashIconProps = {
-    onClick: () => void;
+    onClick?: () => void;
     title?: string;
 }
 
 export default function TrashIcon({onClick, title}:TrashIconProps){
     return (
-        <Button onClick={onClick} title={title ? title : ''}>
-            <DeleteOutlineOutlinedIcon/>
+        <Button onClick={onClick ? onClick : ()=>{}} title={title ? title : ''}>
+            <DeleteOutlineOutlinedIcon color='error'/>
         </Button>
     )
 }
--- front/src/hooks/useColumns/index.tsx
+++ front/src/hooks/useColumns/index.tsx
@@ -1,13 +1,14 @@
 import { GridColDef } from "@mui/x-data-grid";
+import TrashIcon from "../../components/TrashIcon";
 
 export default function useColumns() {
     const columns: GridColDef[] = [
         // { field: 'id_exam', headerName: 'ID do Exame', width: 350 },
-        { field: 'informations', headerName: 'Informações', width: 150 },
-        { field: 'date_time', headerName: 'Data e Hora', width: 150 },
-        { field: 'created_at', headerName: 'Criado', width: 150, },
+        { field: 'informations', headerName: 'Informações', width: 350 },
+        { field: 'date_time', headerName: 'Data e Hora', width: 250 },
+        { field: 'created_at', headerName: 'Criado', width: 250, },
         { field: '', headerName: 'Ações', width: 150, renderCell: function(e){
-            return <></>
+            return <TrashIcon title="Excluir"/>
         }}
 
     ];
