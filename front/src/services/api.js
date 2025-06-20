3a871f672da83d39b2146b6e1d0ac81433e78785
Murilo Henrique Russo <79058778+MuriloRusso@users.noreply.github.com>
2025-06-20 11:02:09 -0300

    deploy in progress

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -2,9 +2,12 @@
 import axios from 'axios';
 
 const localUrlApi = 'http://localhost:3000/';
+const VarcelUrlApi = 'https://aspect-gerenciador-exames-api2.vercel.app/';
+
 
 const api = axios.create({
-  baseURL: localUrlApi,
+  // baseURL: localUrlApi,
+  baseURL: VarcelUrlApi,
   headers: {
     'Content-Type': 'application/json',
   },
